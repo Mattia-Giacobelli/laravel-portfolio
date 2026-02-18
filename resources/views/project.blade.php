0c0171ce838c95e92554060e596290ff490d761a
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2026-02-18 18:47:33 +0100

    slyle the project detail page

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -8,19 +8,23 @@
 
 @section('main')
     
-    <div class="container">
+    <div class="container mt-4">
 
         {{-- @dd($project) --}}
         <div class="card d-flex">
             <div class="card-img-top">
                 <img src="{{Vite::asset($project->img)}}" alt="site preview" width="600px">
             </div>
-            <div>
-                <h1> {{$project->name}} </h1>
-                <h3> {{$project->framework}} </h3>
-                <h5> {{$project->languages}} </h5>
-                <h5> {{$project->category}} </h5>
-                <h5> {{$project->descrizione}} </h5>
+            <div class="p-3">
+                <div class="d-flex justify-content-between">
+
+                    <h1> {{$project->name}} </h1>
+                    <h2> {{$project->category}} </h2>
+
+                </div>
+                <h3> Ramework utilizzati: {{$project->framework}} </h3>
+                <h5> Linguaggi: {{$project->languages}} </h5>
+                <h5 class="mt-5"> {{$project->descrizione}} </h5>
             </div>
         </div>
 
